711c11c18b2a0e87b96ecb453860744cadae8202
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-04-24 21:19:58 -0700

    implement init and fetch methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,56 @@
-// YOUR CODE HERE:
 
+var app;
+
+$(function() {
+  app = {
+
+    server: 'https//api.parse.com/1/classes/chatterbox/',
+    username: 'anonymous',
+
+    init: function() {
+      app.username = window.location.search.substr(10);
+      app.fetch();
+    },
+
+    send: function() {
+
+    },
+
+    fetch: function() {
+      $.ajax({
+        url: app.server,
+        type: 'GET',
+        contentType: 'application/json',
+        data: {
+          order: '-createdAt'
+        },
+        success: function(data) {
+          console.log(data);
+        },
+        error: function(reason) {
+          console.log('failed to fetch data: ', reason);
+        }
+      });
+    },
+
+    clearMessages: function() {
+
+    },
+
+    addMessage: function() {
+
+    },
+
+    addRoom: function() {
+
+    },
+
+    addFriend: function() {
+
+    },
+
+    handleSubmit: function() {
+
+    }
+  };
+});
