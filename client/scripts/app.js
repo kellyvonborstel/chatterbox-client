c27c02f7608d124e277d7016ae425b8aea3a129c
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-04-25 22:40:10 -0700

    implement populateRooms method

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,9 +6,17 @@ $(function() {
 
     server: 'https//api.parse.com/1/classes/chatterbox/',
     username: 'anonymous',
+    room: 'lobby',
 
     init: function() {
       app.username = window.location.search.substr(10);
+
+      app.$main = $('#main');
+      app.$message = $('#message');
+      app.$chat = $('#chat');
+      app.$roomSelect = $('#roomSelect');
+      app.$send = $('#send');
+
       app.fetch();
     },
 
@@ -25,12 +33,32 @@ $(function() {
           order: '-createdAt'
         },
         success: function(data) {
-          console.log(data);
+          app.populateRooms(data.results);
+          app.populateMessages(data.results);
         },
         error: function(reason) {
           console.log('failed to fetch data: ', reason);
         }
       });
+    },
+
+    populateRooms: function(results) {
+      app.$roomSelect.html('<option value="__newRoom">New Room...</option><option value="lobby" selected>Lobby</option>');
+      if (results) {
+        var processsedRooms = {};
+        results.forEach(function(data) {
+          var roomname = data.roomname;
+          if (roomname && !processedRooms[roomname]) {
+            app.addRoom(roomname);
+            processedRooms[roomname] = true;
+          }
+        });
+      }
+      app.$roomSelect.val(app.room);
+    },
+
+    populateMessages: function(results) {
+      
     },
 
     clearMessages: function() {
@@ -41,8 +69,9 @@ $(function() {
 
     },
 
-    addRoom: function() {
-
+    addRoom: function(roomname) {
+      var $option = $('<option />').val(roomname).text(roomname);
+      app.$roomSelect.append($option);
     },
 
     addFriend: function() {
