5850690660fd40d8d041ac0b01bde5d3465bd921
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-04-25 23:47:09 -0700

    implement send method

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -17,14 +17,52 @@ $(function() {
       app.$roomSelect = $('#roomSelect');
       app.$send = $('#send');
 
+      app.$roomSelect.on('change', app.saveRoom);
+      app.$send.on('submit', app.handleSubmit);
+
+      app.stopSpinner();
       app.fetch();
+
+      setInterval(app.fetch, 3000);
     },
 
-    send: function() {
+    saveRoom: function(event) {
+      var selectedIndex = app.$roomSelect.prop('selectedIndex');
+      if (selectedIndex === 0) {
+        var roomname = prompt('Enter a room name');
+        if (roomname) {
+          app.room = roomname;
+          app.addRoom(roomname);
+          app.$roomSelect.val(roomname);
+          app.fetch();
+        }
+      } else {
+        app.room = app.$roomSelect.val();
+        app.fetch();
+      }
+    },
 
+    send: function(data) {
+      app.startSpinner();
+      $.ajax({
+        url: app.server,
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application.json',
+        success: function(result) {
+          app.fetch();
+        },
+        error: function(reason) {
+          console.error('Failed to send data: ', reason);
+        },
+        complete: function() {
+          app.stopSpinner();
+        }
+      });
     },
 
     fetch: function() {
+      app.startSpinner();
       $.ajax({
         url: app.server,
         type: 'GET',
@@ -32,20 +70,35 @@ $(function() {
         data: {
           order: '-createdAt'
         },
+        complete: function() {
+          app.stopSpinner();
+        },
         success: function(data) {
           app.populateRooms(data.results);
           app.populateMessages(data.results);
         },
         error: function(reason) {
-          console.log('failed to fetch data: ', reason);
+          console.error('failed to fetch data: ', reason);
         }
       });
+    },
+
+    startSpinner: function() {
+      $('.spinner img').show();
+    },
+
+    stopSpinner: function() {
+      $('.spinner img').hide();
     },
 
     populateRooms: function(results) {
       app.$roomSelect.html('<option value="__newRoom">New Room...</option><option value="lobby" selected>Lobby</option>');
       if (results) {
         var processsedRooms = {};
+        if (app.room !== 'lobby') {
+          app.addRoom(app.room);
+          processedRooms[app.room] = true;
+        }
         results.forEach(function(data) {
           var roomname = data.roomname;
           if (roomname && !processedRooms[roomname]) {
@@ -93,8 +146,14 @@ $(function() {
 
     },
 
-    handleSubmit: function() {
-
+    handleSubmit: function(event) {
+      event.preventDefault();
+      var message = {
+        username: app.username,
+        roomname: app.room || 'lobby',
+        text: app.$message.val()
+      };
+      app.send(message);
     }
   };
 });
